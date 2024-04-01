59d68fc025fadc5aa13a98ad09988fe3ca6bcd31
kaloa2025 <aalokchoudhari2021@gmail.com>
2024-04-01 09:15:54 +0530

    upload

--- index.js
+++ index.js
@@ -3,7 +3,21 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
+import { Text , View} from 'react-native';
+import Header from './src/Components/Header.js';
+import CarList from './src/Components/CarList.js';
+
+const App =()=>{
+    // return <Text>Hello</Text>
+    // return <Header/>
+    return (
+        <View>
+            <Header title="CARS"/>
+            <CarList/>
+        </View>
+    )
+    
+}
 
 AppRegistry.registerComponent(appName, () => App);
